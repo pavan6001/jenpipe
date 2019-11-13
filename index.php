e24ca8d30f191aebd6d3aa0ea6b8b4eecc84493e
Pavan BN <Pavanbn@v2soft.in>
2019-11-13 20:11:59 +0530

    Revert "Update index.php" conflicts resolved This reverts commit bce25629d1a1f493d50ebc75d16ae96469d62445.

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <html>
  <head>
-  <title>PHP Server</title>
+  <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<h1>Server Deployment Successfull!!</h1>'
-       echo '<h2>Welcome To Jenkins Automation!!</h2>'; ?> 
+ <?php echo '<h1>Server Deployment Successfull!!</h1>'; ?>
+ <?php echo '<h2>Welcome To Jenkins Automation!!</h2>'; ?> 
+ <?php echo '<p>Hello World</p>'; ?> 
  </body>
 </html>
