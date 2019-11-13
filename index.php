e431044722f89a091c5e51e22a202673f47e970e
Pavan BN <Pavanbn@v2soft.in>
2019-11-13 20:03:18 +0530

    modified index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>PHP Server</title>
  </head>
  <body>
- <?php echo '<h1>Server Deployment Successfull!!</h1>'; ?> 
+ <?php echo '<h1>Server Deployment Successfull!!</h1>'
+       echo '<h2>Welcome To Jenkins Automation!!</h2>'; ?> 
  </body>
 </html>
